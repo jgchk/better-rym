eab205903cd6736c8f183e8e51f95e0219d9a124
Jake Cheek <jake@f-m.fm>
2020-02-01 16:57:09 -0500

    use webpack-userscript for bundling

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const WebpackUserscript = require('webpack-userscript')
+const dev = process.env.NODE_ENV === 'development'
+
+module.exports = {
+  mode: dev ? 'development' : 'production',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    filename: 'betterRYM.user.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new WebpackUserscript({
+      headers: {
+        name: 'betterRYM',
+        author: 'mocha',
+        version: dev ? `[version]-build.[buildTime]` : `[version]`,
+      },
+    }),
+  ],
+}
