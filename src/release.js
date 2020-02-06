7754de0b714ce24844a63a51d879ae0d37100e54
Jake Cheek <jake@f-m.fm>
2020-02-05 21:45:22 -0500

    add loading spinner

--- src/release.js
+++ src/release.js
@@ -2,6 +2,7 @@ import apis from './api'
 import '../res/css/release.css'
 
 const releaseUrl = 'https://rateyourmusic.com/release'
+const spinnerClass = 'spinner'
 
 export default function checkReleasePage () {
   if (isReleasePage()) {
@@ -17,28 +18,59 @@ function isReleasePage () {
 }
 
 async function modifyReleasePage () {
-  const info = getReleaseInfo()
+  showStreamLinksBox(true)
+  showLoading(true)
 
+  const info = getReleaseInfo()
   const missingSources = getMissingSources()
-  for (const missingSource of missingSources) {
-    const api = apis[missingSource]
-    const searchResult = await api.search(info.title, info.artist, info.type)
-    if (!searchResult) continue
-    makeSureStreamLinksBoxExists()
-    const $button = $(`<a target="_blank" rel="noopener nofollow" title="${missingSource}" class="ui_stream_link_btn ui_stream_link_btn_${missingSource.toLowerCase()}" href="${searchResult.source}"><i class="fa fa-${missingSource.toLowerCase()}"></i></a>`)
-    $button.addClass('brym')
-    $('.ui_stream_links').find('div[style^="clear:both;"]').before($button)
+  const promises = missingSources.map(source => searchSource(source, info.title, info.artist, info.type))
+  await Promise.all(promises)
+
+  showLoading(false)
+  const $streamLinkButtons = $('.ui_stream_link_btn')
+  if ($streamLinkButtons.length === 0) { // only remove if there are no buttons inside
+    showStreamLinksBox(false)
   }
 }
 
-function makeSureStreamLinksBoxExists () {
+function showStreamLinksBox (show) {
   const $streamLinks = $('.ui_stream_links')
-  if (!$streamLinks.length) {
-    const $streamLinksBox = $(`
+  if (show) {
+    if (!$streamLinks.length) {
+      const $streamLinksBox = $(`
       <div class="ui_stream_links promoted" style="width:1398px;margin:0 auto;margin-top:1em;max-width:350px;">
             <span class="ui_stream_label">Listen:</span>
             <div style="clear:both;"></div></div>`)
-    $('div.hide-for-small > div[style^="margin-top:8px;"]').after($streamLinksBox)
+      $('div.hide-for-small > div[style^="margin-top:8px;"]').after($streamLinksBox)
+    }
+  } else {
+    $streamLinks.remove()
+  }
+}
+
+function showLoading (show) {
+  if (show) {
+    const $spinContainer = $('<div>')
+    $spinContainer.addClass([spinnerClass, 'ui_stream_link_btn', 'brym'])
+    const $spinner = $('<div>')
+    $spinner.addClass(`${spinnerClass}-icon`)
+    $spinContainer.append($spinner)
+    $('.ui_stream_links').find('div[style^="clear:both;"]').before($spinContainer)
+  } else {
+    $(`.ui_stream_links .${spinnerClass}`).remove()
+  }
+}
+
+async function searchSource (source, title, artist, type) {
+  const api = apis[source]
+  const searchResult = await api.search(title, artist, type)
+  if (searchResult) {
+    const $button = $(`<a target="_blank" rel="noopener nofollow" title="${source}" class="ui_stream_link_btn ui_stream_link_btn_${source.toLowerCase()}" href="${searchResult.source}"><i class="fa fa-${source.toLowerCase()}"></i></a>`)
+    $button.addClass('brym')
+    $('.ui_stream_links').find(`.${spinnerClass}`).before($button)
+    return true
+  } else {
+    return false
   }
 }
 
