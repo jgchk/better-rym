e1fbd2978012c8c9d861eeb3ef1cad896e83c78a
Jake Cheek <jake@f-m.fm>
2020-02-06 10:13:52 -0500

    parse new youtube search result format

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,25 +1,24 @@
-import { formatDate } from '../lib/time'
 import { getMostSimilar } from '../lib/string'
 
 const searchUrl = query => `https://api.jake.cafe/youtube/search?q=${encodeURIComponent(query)}`
-const videoUrl = id => `https://www.youtube.com/watch?v=${id}`
+const infoUrl = id => `https://api.jake.cafe/youtube/video/${id}`
 
-async function search (title, artist, type) {
+function search (title, artist, type) {
   const query = `${artist} ${title}`
   return new Promise((resolve, reject) => {
     GM_xmlhttpRequest({
       method: 'GET',
       url: searchUrl(query),
       responseType: 'json',
-      onload: result => {
+      onload: async result => {
         if (result.status === 200) {
-          const results = result.response.data.items
+          const results = result.response.videos
           if (!results || results.length === 0) {
             resolve(null)
           } else {
             const topResult = getBestMatch(title, artist, results)
             if (topResult) {
-              resolve(parseObject(topResult))
+              resolve(await getInfo(topResult.videoId))
             } else {
               resolve(null)
             }
@@ -36,16 +35,35 @@ async function search (title, artist, type) {
 function getBestMatch (title, artist, items) {
   const mainString = `${artist} ${title}`
   const thresholdSimilarity = 0.5
-  return getMostSimilar(mainString, items, thresholdSimilarity, item => `${item.snippet.title}`)
+  return getMostSimilar(mainString, items, thresholdSimilarity, item => `${item.title}`)
+}
+
+function getInfo (id) {
+  return new Promise((resolve, reject) => {
+    GM_xmlhttpRequest({
+      method: 'GET',
+      url: infoUrl(id),
+      responseType: 'json',
+      onload: result => {
+        if (result.status === 200) {
+          const info = parseObject(result.response)
+          resolve(info)
+        } else {
+          reject(result.status)
+        }
+      },
+      onerror: error => reject(error)
+    })
+  })
 }
 
 function parseObject (object) {
   const info = {}
-  info.title = object.snippet.title
+  info.title = object.title
   info.format = 'digital file'
   info.attributes = 'streaming'
-  info.source = videoUrl(object.id.videoId)
-  info.date = formatDate(object.snippet.publishedAt)
+  info.source = object.url
+  info.date = object.uploadDate
   return info
 }
 
