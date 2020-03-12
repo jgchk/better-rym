a729e48e2253927e8f8e994c9225510925bfa5d6
Jake Cheek <jake@f-m.fm>
2020-03-11 23:24:08 -0400

    add collection filter buttons

--- src/lib/path.js
+++ src/lib/path.js
@@ -1,8 +1,15 @@
 import { arraysEqual } from './array'
 
-// eslint-disable-next-line import/prefer-default-export
+export function splitPath() {
+  return window.location.pathname.split('/')
+}
+
+export function joinPath(path) {
+  return `/${path.join('/')}`
+}
+
 export function inPath(...path) {
-  const currentPath = window.location.pathname.split('/')
+  const currentPath = splitPath()
   if (currentPath.length - 1 < path.length) return false
   return arraysEqual(path, currentPath.slice(1, path.length + 1))
 }
