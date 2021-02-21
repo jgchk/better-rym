e9452fdeb1d46d100fd08c9101d6da6d0cd1b892
Jake Cheek <jake@f-m.fm>
2021-02-20 19:57:33 -0500

    🔧 split up webpack configs

--- webpack.common.js
+++ webpack.common.js
@@ -1,18 +1,10 @@
 const path = require('path')
-const process = require('process')
-
 const { CleanWebpackPlugin: CleanPlugin } = require('clean-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const DotEnv = require('dotenv-webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const CssoWebpackPlugin = require('csso-webpack-plugin').default
-
-const isProduction = process.env.NODE_ENV === 'production'
-const isDevelopment = !isProduction
-const mode = isProduction ? 'production' : 'development'
 
 module.exports = {
-  mode,
   entry: {
     release: './src/release/index.tsx',
     import: './src/import/index.tsx',
@@ -29,11 +21,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          { loader: 'css-loader', options: { sourceMap: isDevelopment } },
-          'postcss-loader',
-        ],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
       },
       {
         test: /\.svg$/,
@@ -62,7 +50,5 @@ module.exports = {
     new DotEnv(),
     new CopyPlugin({ patterns: [{ from: './manifest.json' }] }),
     new MiniCssExtractPlugin({ filename: '[name].css' }),
-    new CssoWebpackPlugin(),
   ],
-  devtool: isDevelopment && 'eval-cheap-module-source-map',
 }
