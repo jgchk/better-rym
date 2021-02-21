7dac02cc04ccb41c8ea368f6d40807e0ce87b012
Jake Cheek <jake@f-m.fm>
2021-02-20 21:31:07 -0500

    👷 auto reload on dev build

--- webpack.development.js
+++ webpack.development.js
@@ -1,7 +1,18 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common')
+const ExtensionReloader = require('webpack-extension-reloader')
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'eval-cheap-module-source-map',
+  plugins: [
+    new ExtensionReloader({
+      port: 9090,
+      reloadPage: true,
+      entries: {
+        contentScript: ['release', 'import'],
+        background: 'background',
+      },
+    }),
+  ],
 })
