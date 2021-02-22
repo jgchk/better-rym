b08a3b47014b382482f1b466d71ee3ed8f9fc92f
Jake Cheek <jake@f-m.fm>
2021-02-21 19:36:24 -0500

    💄 show errors in tooltip

--- src/common/components/failed.tsx
+++ src/common/components/failed.tsx
@@ -1,10 +1,32 @@
 import clsx from 'clsx'
 import { FunctionComponent, JSX, h } from 'preact'
+import { createPortal } from 'preact/compat'
+import { useState } from 'preact/hooks'
 import Icon from '../../../res/x.svg'
 import styles from '../styles/failed.module.css'
 
 export const Failed: FunctionComponent<
   { error: Error } & JSX.SVGAttributes<SVGSVGElement>
-> = ({ error, className, ...properties }) => (
-  <Icon className={clsx(styles.failed, className)} {...properties} />
-)
+> = ({ error, className, ...properties }) => {
+  const [showTip, setShowTip] = useState(false)
+  const [pos, setPos] = useState({ x: 0, y: 0 })
+
+  return (
+    <>
+      <Icon
+        onMouseOver={() => setShowTip(true)}
+        onMouseOut={() => setShowTip(false)}
+        onMouseMove={(event) => setPos({ x: event.pageX, y: event.pageY })}
+        className={clsx(styles.failed, className)}
+        {...properties}
+      />
+      {showTip &&
+        createPortal(
+          <div className={styles.tooltip} style={{ left: pos.x, top: pos.y }}>
+            {error.message}
+          </div>,
+          document.body
+        )}
+    </>
+  )
+}
