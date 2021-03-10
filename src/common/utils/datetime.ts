0a0f8d81233f56f9ef11d738ad689303ab251f8d
Jake Cheek <jake@f-m.fm>
2021-03-10 16:21:10 -0500

    🐛 fix off-by-one date FOR REAL

--- src/common/utils/datetime.ts
+++ src/common/utils/datetime.ts
@@ -5,7 +5,7 @@ export const stringToDate = (dateString: string): ReleaseDate => {
   return {
     year: date.getUTCFullYear(),
     month: date.getUTCMonth() + 1,
-    day: date.getUTCDate() + 1,
+    day: date.getUTCDate(),
   }
 }
 
