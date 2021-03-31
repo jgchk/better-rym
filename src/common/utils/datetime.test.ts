37e12b7f44ce490c44a8d8d3649fb9b3f3774c41
Jake Cheek <jake@f-m.fm>
2021-03-31 07:21:54 -0400

    🐛 fix time rounding issue

---
+++ src/common/utils/datetime.test.ts
@@ -0,0 +1,8 @@
+import { secondsToString } from './datetime'
+
+describe('secondsToString', () => {
+  test('gets rounding correct', () => {
+    expect(secondsToString(59.4)).toBe('0:59')
+    expect(secondsToString(59.5)).toBe('1:00')
+  })
+})
--- src/common/utils/datetime.ts
+++ src/common/utils/datetime.ts
@@ -10,7 +10,8 @@ export const stringToDate = (dateString: string): ReleaseDate => {
 }
 
 export const secondsToString = (seconds: number): string => {
+  seconds = Math.round(seconds)
   const min = Math.floor(seconds / 60)
-  const sec = Math.round(seconds % 60)
+  const sec = Math.floor(seconds % 60)
   return `${min}:${sec.toString().padStart(2, '0')}`
 }
