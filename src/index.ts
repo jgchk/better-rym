4f34470a729624232306e15525004b9819ffe203
Jake Cheek <jake@f-m.fm>
2021-06-28 11:25:18 -0400

    ⚡️ the big one - use one file for everything to drastically reduce bundle size

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+if (location.pathname.startsWith('/release')) {
+  void import('./find-stream-links')
+} else if (location.pathname.startsWith('/releases/ac')) {
+  void import('./release-submission')
+} else if (location.pathname.startsWith('/images/upload')) {
+  void import('./cover-art')
+} else if (location.pathname.startsWith('/submit_media_link')) {
+  void import('./submit-stream-links')
+} else if (location.pathname.startsWith('/collection')) {
+  void import('./filter-collection')
+}
