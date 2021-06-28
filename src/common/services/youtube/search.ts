4045546d72ebac13ca685b89bc5f8fb5c9a3725e
Jake Cheek <jake@f-m.fm>
2021-06-28 10:01:59 -0400

    🐛 fix youtube search

--- src/common/services/youtube/search.ts
+++ src/common/services/youtube/search.ts
@@ -12,8 +12,16 @@ export const search: SearchFunction = async ({ artist, title }) => {
     },
   })
 
+  const html = new DOMParser().parseFromString(response, 'text/html')
+  const scripts = [...html.getElementsByTagName('script')]
+  const dataScript = scripts.find((script) =>
+    script.textContent?.includes('var ytInitialData')
+  )?.textContent
+  if (dataScript === null || dataScript === undefined)
+    throw new Error('Could not find search metadata tag')
+
   const searchObjectString = [
-    ...(/var ytInitialData = (.*);<\/script>/.exec(response) ?? []),
+    ...(/var ytInitialData = ({.*});/.exec(dataScript) ?? []),
   ][1]
   if (isUndefined(searchObjectString))
     throw new Error('Could not find search metadata')
@@ -24,7 +32,8 @@ export const search: SearchFunction = async ({ artist, title }) => {
       .sectionListRenderer.contents[0]?.itemSectionRenderer?.contents
   if (isUndefined(videoData)) throw new Error('Could not find video data')
 
-  const videoId = videoData.map((video) => video.videoRenderer).find(isDefined)
-    ?.videoId
+  const videoId = videoData
+    .map((video) => video.videoRenderer)
+    .find(isDefined)?.videoId
   return videoId ? `https://www.youtube.com/watch?v=${videoId}` : undefined
 }
