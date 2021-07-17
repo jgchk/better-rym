bd1730a576ebd7fb8866bc33ae0b7eca1b16cb04
Jake Cheek <jake@f-m.fm>
2021-07-17 15:33:55 -0400

    🐛 fix melon regex

--- src/common/services/melon/index.ts
+++ src/common/services/melon/index.ts
@@ -6,7 +6,7 @@ import { resolve } from './resolve'
 export const Melon: Service & Resolvable = {
   id: 'melon',
   name: 'Melon',
-  regex: /https?:\/\/www.melon.com\/album\/detail.htm?albumId=\d+#/,
+  regex: /https?:\/\/www\.melon\.com\/album\/detail\.htm\?albumId=\d+/,
   icon: Icon,
   resolve: withCache('melon', 'resolve', resolve),
 }
