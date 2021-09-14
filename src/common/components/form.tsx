c7b2cd4e7a9d6b288a2be8923367ad7bb2dcdec2
lempamo <lempamo@outlook.com>
2021-09-14 17:47:51 -0400

    select which fields you want filled from a source

--- src/common/components/form.tsx
+++ src/common/components/form.tsx
@@ -5,12 +5,11 @@ import {
   CAPITALIZATION_TYPES,
   CapitalizationType,
 } from '../../release-submission/utils/capitalization'
+import { ReleaseOptions } from '../../release-submission/utils/types'
 import { getMatchingService } from '../services'
 import { Service } from '../services/types'
 import styles from '../styles/form.module.css'
 import { ServiceSelector } from './service-selector'
-
-const CAPITALIZE_ID = 'brym-capitalize'
 
 const CAPITALIZATION_TYPE_MAP: Record<CapitalizationType, string> = {
   'title-case': 'Title Case',
@@ -18,14 +17,22 @@ const CAPITALIZATION_TYPE_MAP: Record<CapitalizationType, string> = {
   'as-is': 'Keep as-is',
 }
 
+const FIELDS_MAP: Record<string, string> = {
+  artists: 'Artists',
+  type: 'Release Type',
+  date: 'Release Date',
+  title: 'Title',
+  format: 'Issued Format',
+  discSize: 'Disc Size',
+  label: 'Label',
+  attributes: 'Issue Attributes',
+  tracks: 'Tracklist',
+}
+
 export type Properties<S extends Service> = {
   services: S[]
   submitText: string
-  onSubmit: (
-    url: string,
-    service: S,
-    capitalization: CapitalizationType
-  ) => void
+  onSubmit: (url: string, service: S, options: ReleaseOptions) => void
   showAutoCapitalize?: boolean
 }
 
@@ -35,6 +42,10 @@ export const Form = <S extends Service>({
   onSubmit,
   showAutoCapitalize = false,
 }: Properties<S>): VNode => {
+  const _fillers: { [x: string]: boolean } = {}
+  Object.keys(FIELDS_MAP).map((field) => (_fillers[field] = true))
+
+  const [fillers, setFillers] = useState(_fillers)
   const [url, setUrl] = useState('')
   const [selectedService, setSelectedService] = useState<S | undefined>(
     undefined
@@ -60,7 +71,10 @@ export const Form = <S extends Service>({
       onSubmit={(event) => {
         event.preventDefault()
         if (selectedService !== undefined) {
-          onSubmit(url, selectedService, capitalization)
+          onSubmit(url, selectedService, {
+            capitalization: capitalization,
+            fillFields: fillers,
+          } as ReleaseOptions)
         } else {
           setShowMissingServiceError(true)
         }
@@ -82,24 +96,52 @@ export const Form = <S extends Service>({
           <div className={styles.error}>Select an import source</div>
         )}
         {showAutoCapitalize && (
-          <label htmlFor={CAPITALIZE_ID}>
-            Capitalization{' '}
-            <select
-              value={capitalization}
-              onChange={(event) =>
-                setCapitalization(
-                  (event.target as HTMLSelectElement)
-                    .value as CapitalizationType
-                )
-              }
+          <details>
+            <summary style='text-align:center'>Advanced Options</summary>
+            <div
+              id='brym-release-options'
+              style='margin-top:0.5em;'
+              className={styles.input}
             >
-              {CAPITALIZATION_TYPES.map((capType) => (
-                <option value={capType} key={capType}>
-                  {CAPITALIZATION_TYPE_MAP[capType]}
-                </option>
+              <hr style='width:100%;margin-bottom:0.5em' />
+              <label htmlFor='brym-capitalize'>
+                Capitalization:&nbsp;
+                <select
+                  value={capitalization}
+                  onChange={(event) =>
+                    setCapitalization(
+                      (event.target as HTMLSelectElement)
+                        .value as CapitalizationType
+                    )
+                  }
+                >
+                  {CAPITALIZATION_TYPES.map((capType) => (
+                    <option value={capType} key={capType}>
+                      {CAPITALIZATION_TYPE_MAP[capType]}
+                    </option>
+                  ))}
+                </select>
+              </label>
+              {Object.keys(FIELDS_MAP).map((field) => (
+                <label key={field}>
+                  {FIELDS_MAP[field]}:&nbsp;
+                  <input
+                    id={`brym-${field}`}
+                    type='checkbox'
+                    checked={fillers[field]}
+                    onChange={(event) =>
+                      setFillers((previousState) => {
+                        const checkbox = event.target as HTMLInputElement
+                        const newState = previousState
+                        newState[checkbox.id.slice(5)] = checkbox.checked
+                        return newState
+                      })
+                    }
+                  />
+                </label>
               ))}
-            </select>
-          </label>
+            </div>
+          </details>
         )}
       </div>
       <input type='submit' value={submitText} className={styles.submit} />
---
+++ src/release-submission/utils/types.ts
@@ -0,0 +1,16 @@
+import { CapitalizationType } from './capitalization'
+
+export type ReleaseOptions = {
+  capitalization: CapitalizationType
+  fillFields: {
+    artists: boolean
+    type: boolean
+    date: boolean
+    title: boolean
+    format: boolean
+    discSize: boolean
+    attributes: boolean
+    tracks: boolean
+    label: boolean
+  }
+}
