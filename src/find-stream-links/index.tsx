b170652ed89695b5c7b6dab1b4170975ba3f4e0e
lempamo <lempamo@outlook.com>
2021-10-26 12:53:43 -0400

    fixes #107 (only works for logged-in users though)

--- src/find-stream-links/index.tsx
+++ src/find-stream-links/index.tsx
@@ -6,13 +6,15 @@ import { App } from './components/app'
 const main = async () => {
   const siblingElement = await waitForElement(
     '.hide-for-small [style="margin-top:8px;"]'
-  ).catch(() => document.querySelector('.hide-for-small a[href^="buy"]'))
+  ).catch(() =>
+    document.querySelector('.hide-for-small a[href^="/submit_media_link"]')
+  )
   if (!siblingElement)
     throw new Error('Could not find insertion point for BetterRYM')
 
   const app = document.createElement('div')
   app.id = 'better-rym'
-  siblingElement.after(app)
+  siblingElement.before(app)
 
   render(<App />, app)
 }
