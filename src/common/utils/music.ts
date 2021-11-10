a50a3f74034216fec4c48ace0b6ae8c5e79b6e24
Jake Cheek <jake@f-m.fm>
2021-11-10 11:11:42 -0500

    report 7 track releases as eps

--- src/common/utils/music.ts
+++ src/common/utils/music.ts
@@ -2,6 +2,6 @@ import { ReleaseType } from '../services/types'
 
 export const getReleaseType = (numberOfTracks: number): ReleaseType => {
   if (numberOfTracks <= 3) return 'single'
-  if (numberOfTracks <= 7) return 'ep'
+  if (numberOfTracks <= 6) return 'ep'
   return 'album'
 }
