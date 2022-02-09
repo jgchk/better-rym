0c7591939f49339d3ee8a0853c47267e18b8fe9d
Jake Cheek <jake@f-m.fm>
2022-02-09 09:34:19 -0500

    don't show tab icon on non BRYM-pages

--- src/modules/background/index.ts
+++ src/modules/background/index.ts
@@ -43,26 +43,14 @@ const setTabIcon = (tabId: number, enabled: boolean) => {
 }
 
 browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
-  void browser.pageAction.show(id)
   const pageUrls = Object.values(pages)
   for (const pageUrl of pageUrls) {
     if (tab.url && new URL(tab.url).pathname.startsWith(pageUrl)) {
       void getPageEnabled(pageUrl).then((enabled) => setTabIcon(id, enabled))
+      void browser.pageAction.show(id)
       return
     }
   }
-
-  void browser.pageAction.setIcon({
-    tabId: id,
-    path: {
-      '19': 'extension-neutral-19.png',
-      '38': 'extension-neutral-38.png',
-    },
-  })
-  void browser.pageAction.setTitle({
-    tabId: id,
-    title: 'BetterRYM',
-  })
 })
 
 browser.pageAction.onClicked.addListener((tab) => {
