42537becdf444080cdc87a4175cc36f83da1a534
Jake Cheek <jake@f-m.fm>
2022-05-07 12:32:16 -0400

    fix selectShortcut for artists with quotes in name

--- src/modules/release-submission/utils/page-functions.ts
+++ src/modules/release-submission/utils/page-functions.ts
@@ -5,7 +5,8 @@ export const selectShortcut = (
   id: number,
   name: string,
   target: string
-): void => runScript(`selectShortcut('${type}', ${id}, '${name}', '${target}')`)
+): void =>
+  runScript(`selectShortcut(\`${type}\`, ${id}, \`${name}\`, \`${target}\`)`)
 
 // window.parent.goInfobox(897)
 export const goInfobox = (id: number): void => runScript(`goInfobox(${id})`)
