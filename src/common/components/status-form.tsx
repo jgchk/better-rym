3d2d7deddbcf26ac771c3641c3908385477c56f6
Jake Cheek <jake.cheek@itential.com>
2022-05-14 14:48:50 -0400

    log full errors in status form

--- src/common/components/status-form.tsx
+++ src/common/components/status-form.tsx
@@ -1,9 +1,10 @@
 import { h, VNode } from 'preact'
+import { useEffect } from 'preact/hooks'
 
 import { ReleaseOptions } from '../../modules/release-submission/utils/types'
 import { Service } from '../services/types'
 import styles from '../styles/status-form.module.css'
-import { fold, OneShot } from '../utils/one-shot'
+import { fold, isFailed, OneShot } from '../utils/one-shot'
 import { pipe } from '../utils/pipe'
 import { Complete } from './complete'
 import { Failed } from './failed'
@@ -28,22 +29,32 @@ export const StatusForm = <E extends Error, T, S extends Service>({
   onSubmit,
   submitText = 'Submit',
   showAutoCapitalize = false,
-}: Properties<E, T, S>): VNode => (
-  <div className={styles.container}>
-    <Form
-      services={services}
-      submitText={submitText}
-      onSubmit={(url, service, options) => void onSubmit(url, service, options)}
-      showAutoCapitalize={showAutoCapitalize}
-    />
-    {pipe(
-      data,
-      fold(
-        () => null,
-        () => <Loader />,
-        (error) => <Failed error={error} />,
-        () => <Complete />
-      )
-    )}
-  </div>
-)
+}: Properties<E, T, S>): VNode => {
+  useEffect(() => {
+    if (isFailed(data)) {
+      console.error(data.error)
+    }
+  }, [data])
+
+  return (
+    <div className={styles.container}>
+      <Form
+        services={services}
+        submitText={submitText}
+        onSubmit={(url, service, options) =>
+          void onSubmit(url, service, options)
+        }
+        showAutoCapitalize={showAutoCapitalize}
+      />
+      {pipe(
+        data,
+        fold(
+          () => null,
+          () => <Loader />,
+          (error) => <Failed error={error} />,
+          () => <Complete />
+        )
+      )}
+    </div>
+  )
+}
