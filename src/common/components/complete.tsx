df1b90e1990107aefb14007f90b0c48736dbe89a
Elliot Chance <elliotchance@gmail.com>
2023-05-15 21:33:22 -0400

    Spotify: Fix missing artists
    Will now include artist(s) from Spotify. There doesn't seem to be any
    information on how they should join so we'll have to assume a comma.
    
    Also, had to fix some types to get it to compile.
    
    Fixes #180

--- src/common/components/complete.tsx
+++ src/common/components/complete.tsx
@@ -2,9 +2,9 @@ import { FunctionComponent, h, JSX } from 'preact'
 
 import Icon from '../../../res/svg/check.svg'
 import styles from '../styles/complete.module.css'
-import { clsx } from '../utils/clsx'
+import { ClassValue, clsx } from '../utils/clsx'
 
 export const Complete: FunctionComponent<JSX.SVGAttributes<SVGSVGElement>> = ({
   className,
   ...properties
-}) => <Icon className={clsx(styles.complete, className)} {...properties} />
+}) => <Icon className={clsx(styles.complete, className as ClassValue)} {...properties} />
