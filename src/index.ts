6d04040bbb427587c341c681ca91994f16455087
Jake Cheek <jake@f-m.fm>
2024-09-26 21:02:40 -0400

    move user page to separate entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { getPageEnabled, pages } from './common/pages'
 import { main as searchBar } from './modules/search-bar'
 import { main as streamLinkMissing } from './modules/stream-link-missing'
-import { main as userPage } from './modules/user-page'
 import { main as voteHistoryDescriptors } from './modules/vote-history/descriptors'
 import { main as voteHistoryGenres } from './modules/vote-history/genres'
 
@@ -15,7 +14,6 @@ const runPage = async (page: string, callback: () => unknown) => {
   callback()
 }
 
-void runPage(pages.userPage, userPage)
 void runPage(pages.voteHistoryGenres, voteHistoryGenres)
 void runPage(pages.voteHistoryDescriptors, voteHistoryDescriptors)
 void runPage(pages.streamLinkMissing, streamLinkMissing)
