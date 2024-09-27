f46f4d416f5bfc3385c67b33b5e6a48975e0d787
Jake Cheek <jake@f-m.fm>
2024-09-27 16:27:58 -0400

    load css

--- src/modules/cover-art/main.ts
+++ src/modules/cover-art/main.ts
@@ -1,3 +1,12 @@
-import { main } from '.'
+import { main } from ".";
+import browser from "webextension-polyfill";
 
-void main()
+void main();
+
+for (const cssPath of import.meta.PLUGIN_WEB_EXT_CHUNK_CSS_PATHS) {
+  const styles = document.createElement("link");
+  styles.rel = "stylesheet";
+  styles.href = browser.runtime.getURL(cssPath);
+
+  document.head.appendChild(styles);
+}
