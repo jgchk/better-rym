7a8885675d33ec482e5bf8af5a4ca45711876915
Jake Cheek <jake@f-m.fm>
2024-09-27 15:49:23 -0400

    fix soundcloud icons

--- src/common/services/soundcloud/index.ts
+++ src/common/services/soundcloud/index.ts
@@ -1,21 +1,21 @@
-import SpotifyIcon from '../../icons/spotify'
-import SpotifyFoundIcon from '../../icons/spotify-found'
-import SpotifyNotFoundIcon from '../../icons/spotify-notfound'
-import { withCache } from '../../utils/cache'
-import { Embeddable, Resolvable, Searchable, Service } from '../types'
-import { embed } from './embed'
-import { resolve } from './resolve'
-import { search } from './search'
+import SoundcloudIcon from "~/common/icons/soundcloud";
+import { withCache } from "../../utils/cache";
+import { Embeddable, Resolvable, Searchable, Service } from "../types";
+import { embed } from "./embed";
+import { resolve } from "./resolve";
+import { search } from "./search";
+import SoundcloudNotFoundIcon from "~/common/icons/soundcloud-notfound";
+import SoundcloudFoundIcon from "~/common/icons/soundcloud-found";
 
 export const Soundcloud: Service & Searchable & Resolvable & Embeddable = {
-  id: 'soundcloud',
-  name: 'Soundcloud',
+  id: "soundcloud",
+  name: "Soundcloud",
   regex:
     /((http:\/\/(soundcloud\.com\/.*|soundcloud\.com\/.*\/.*|soundcloud\.com\/.*\/sets\/.*|soundcloud\.com\/groups\/.*|snd\.sc\/.*))|(https:\/\/(soundcloud\.com\/.*|soundcloud\.com\/.*\/.*|soundcloud\.com\/.*\/sets\/.*|soundcloud\.com\/groups\/.*)))/,
-  icon: SpotifyIcon,
-  foundIcon: SpotifyFoundIcon,
-  notFoundIcon: SpotifyNotFoundIcon,
-  search: withCache('soundcloud-search', search),
-  resolve: withCache('soundcloud-resolve', resolve),
-  embed: withCache('soundcloud-embed', embed),
-}
+  icon: SoundcloudIcon,
+  foundIcon: SoundcloudFoundIcon,
+  notFoundIcon: SoundcloudNotFoundIcon,
+  search: withCache("soundcloud-search", search),
+  resolve: withCache("soundcloud-resolve", resolve),
+  embed: withCache("soundcloud-embed", embed),
+};
