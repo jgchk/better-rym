32a11b699724e1746ae3606a2e90c3d7b6df5431
Jake Cheek <jake@f-m.fm>
2024-09-28 12:18:31 -0400

    remove unnecessary css module

--- src/common/components/loader.tsx
+++ src/common/components/loader.tsx
@@ -1,11 +1,40 @@
 import { FunctionComponent, h, JSX } from 'preact'
 
 import LoaderIcon from '../icons/loader'
-import styles from '../styles/loader.module.css'
-import { clsx } from '../utils/clsx'
+import { useEffect } from 'preact/hooks'
 
 export const Loader: FunctionComponent<
   Omit<JSX.SVGAttributes<SVGSVGElement>, 'className'> & { className?: string }
-> = ({ className, ...properties }) => (
-  <LoaderIcon className={clsx(styles.loader, className)} {...properties} />
-)
+> = ({ className, ...properties }) => {
+  useEffect(() => {
+    const style = document.createElement('style')
+    style.textContent = keyframes
+    document.head.appendChild(style)
+
+    return () => {
+      document.head.removeChild(style)
+    }
+  }, [])
+
+  return (
+    <LoaderIcon
+      className={className}
+      style={{
+        color: 'var(--mono-5)',
+        animation: 'spin 1.5s linear infinite',
+      }}
+      {...properties}
+    />
+  )
+}
+
+const keyframes = `
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`
