a910041d4e8e27beb1ab35eb92e89a904c572785
Jake Cheek <jake@f-m.fm>
2024-09-28 12:11:19 -0400

    remove unnecessary css module

--- src/common/components/complete.tsx
+++ src/common/components/complete.tsx
@@ -1,11 +1,15 @@
 import { FunctionComponent, h, JSX } from 'preact'
 
 import CheckIcon from '../icons/check'
-import styles from '../styles/complete.module.css'
-import { clsx } from '../utils/clsx'
 
 export const Complete: FunctionComponent<
   Omit<JSX.SVGAttributes<SVGSVGElement>, 'className'> & { className?: string }
 > = ({ className, ...properties }) => (
-  <CheckIcon className={clsx(styles.complete, className)} {...properties} />
+  <CheckIcon
+    className={className}
+    style={{
+      color: 'var(--gen-bg-darkgreen)',
+    }}
+    {...properties}
+  />
 )
