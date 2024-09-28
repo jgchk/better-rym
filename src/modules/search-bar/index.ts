b9875edebdfb57536c29f6bf65aa20cbe9a1a5c5
Jake Cheek <jake@f-m.fm>
2024-09-28 16:13:27 -0400

    reorganize search bar code

--- src/modules/search-bar/index.ts
+++ src/modules/search-bar/index.ts
@@ -1,6 +1,20 @@
-/* eslint-disable unicorn/prefer-add-event-listener */
 import { forceQuerySelector, waitForElement } from '../../common/utils/dom'
 import { parseMarkup } from '../../common/utils/markup'
+
+export async function main() {
+  const searchBar: HTMLInputElement = await waitForElement(
+    '#ui_search_input_main_search',
+  )
+  const searchButton = forceQuerySelector<HTMLElement>(document)(
+    '#ui_search_icon_main_search',
+  )
+
+  keyHandler = searchBar.onkeydown
+  clickHandler = searchButton.onclick
+
+  searchBar.onkeydown = shortcutHandler
+  searchButton.onclick = shortcutHandler
+}
 
 let keyHandler:
   | ((this: GlobalEventHandlers, event: KeyboardEvent) => unknown)
@@ -12,11 +26,11 @@ let clickHandler:
 const SEARCH_REGEX =
   /^\[(Artist|Album|Genre|Label|List|Rating|Venue|Concert|Bug)\d*]$/g
 
-const shortcutHandler = (event: Event) => {
+function shortcutHandler(event: Event) {
   if (event.type == 'keydown' && (event as KeyboardEvent).key != 'Enter') return
 
   const searchBar = forceQuerySelector<HTMLInputElement>(document)(
-    '#ui_search_input_main_search'
+    '#ui_search_input_main_search',
   )
 
   if (SEARCH_REGEX.test(searchBar.value)) {
@@ -29,24 +43,9 @@ const shortcutHandler = (event: Event) => {
     if (event.type == 'click' && clickHandler != null)
       clickHandler.call(
         forceQuerySelector<HTMLElement>(document)(
-          '#ui_search_icon_main_search'
+          '#ui_search_icon_main_search',
         ),
-        event as MouseEvent
+        event as MouseEvent,
       )
   }
-}
-
-export const main = async () => {
-  const searchBar: HTMLInputElement = await waitForElement(
-    '#ui_search_input_main_search'
-  )
-  const searchButton = forceQuerySelector<HTMLElement>(document)(
-    '#ui_search_icon_main_search'
-  )
-
-  keyHandler = searchBar.onkeydown
-  clickHandler = searchButton.onclick
-
-  searchBar.onkeydown = shortcutHandler
-  searchButton.onclick = shortcutHandler
 }
