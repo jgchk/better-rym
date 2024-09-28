73b40596f59fa448208c58efefa524d3e7526b46
Jake Cheek <jake@f-m.fm>
2024-09-28 16:29:21 -0400

    refactor: improve page data

--- src/modules/stream-links/utils/page-data.ts
+++ src/modules/stream-links/utils/page-data.ts
@@ -6,60 +6,66 @@ import {
   waitForElement,
 } from '../../../common/utils/dom'
 
-const getTitle = async () => {
-  const titleElement = await waitForElement<HTMLMetaElement>(
-    'meta[itemprop=name]'
-  )
-  return titleElement.content
+export type Metadata = { artist: string; title: string }
+export type PageData = { metadata: Metadata; links: Links }
+export async function getPageData(): Promise<PageData> {
+  const [artist, title, links] = await Promise.all([
+    getArtist(),
+    getTitle(),
+    getLinks(),
+  ])
+  return { metadata: { artist, title }, links }
 }
 
-const getArtist = async () => {
+async function getArtist() {
   const artistElement = await waitForElement<HTMLAnchorElement>('a.artist')
   return artistElement.text
 }
 
-/* eslint-disable */
-function f(serviceName: any, linkData: any, streamingPreferences: any) {
-  let a = null,
-    n = null
-  for (const r in linkData) {
-    const index = linkData[r]
-    if (index.default) (a = index), (n = r)
-    else if (
-      index.for &&
-      index.for.includes(streamingPreferences.service_regions[serviceName])
-    )
-      return (index.media_id = r), index
-  }
-  return a
-    ? a.not && a.not.includes(streamingPreferences.service_regions[serviceName])
-      ? null
-      : ((a.media_id = n), a)
-    : null
+async function getTitle() {
+  const titleElement = await waitForElement<HTMLMetaElement>(
+    'meta[itemprop=name]',
+  )
+  return titleElement.content
 }
-function m(t: any, linkData: any) {
-  switch (t) {
-    case 'spotify':
-      return `https://open.spotify.com/${linkData.type}/${linkData.media_id}`
-    case 'applemusic':
-      return `https://geo.music.apple.com/${linkData.loc}/${
-        linkData.album ? 'album' : 'video'
-      }/${linkData.album ? linkData.album : linkData.video}/${
-        linkData.media_id
-      }`
-    case 'soundcloud':
-      return `https://${linkData.url}`
-    case 'bandcamp':
-      return `https://${linkData.url}`
-    case 'youtube':
-      return `https://www.youtube.com/watch?v=${linkData.media_id}`
-    default:
-      throw new Error(`Cannot create links for service: ${t}`)
-  }
-}
-/* eslint-enable */
 
-type StreamingPreferences = Record<string, unknown>
+async function getLinks(): Promise<Links> {
+  await waitForDocumentReady()
+
+  const streamingPreferences = await getStreamingPreferences()
+  if (!streamingPreferences) return EMPTY_LINKS
+
+  const element_ = document.querySelector<HTMLElement>(
+    '#media_link_button_container_top',
+  )
+  if (!element_) return EMPTY_LINKS
+
+  const linksString = element_.dataset.links
+  if (!linksString) return EMPTY_LINKS
+
+  const linksData = JSON.parse(linksString)
+
+  const e: { [service: string]: string } = {}
+  for (const service in linksData) {
+    const linkData = linksData[service]
+    const r = f(service, linkData, streamingPreferences)
+    if (r) {
+      e[service] = getFullLink(service, r)
+    }
+  }
+
+  return Object.fromEntries(SEARCHABLES.map(({ id }) => [id, e[id]])) as Record<
+    ServiceId,
+    string | undefined
+  >
+}
+
+export type Links = Record<ServiceId, string | undefined>
+
+const EMPTY_LINKS = Object.fromEntries(
+  SEARCHABLES.map(({ id }) => [id, undefined]),
+) as Record<ServiceId, string | undefined>
+
 const getStreamingPreferences = async (): Promise<
   StreamingPreferences | undefined
 > => {
@@ -83,49 +89,50 @@ const getStreamingPreferences = async (): Promise<
   })
 }
 
-const EMPTY_LINKS = Object.fromEntries(
-  SEARCHABLES.map(({ id }) => [id, undefined])
-) as Record<ServiceId, string | undefined>
+type StreamingPreferences = { service_regions: Record<string, unknown> }
 
-export type Links = Record<ServiceId, string | undefined>
-const getLinks = async (): Promise<Links> => {
-  await waitForDocumentReady()
+function f(
+  service: string,
+  linkData: any,
+  streamingPreferences: StreamingPreferences,
+) {
+  let a = null
+  let n = null
 
-  const streamingPreferences = await getStreamingPreferences()
-  if (!streamingPreferences) return EMPTY_LINKS
-
-  const element_ = document.querySelector<HTMLElement>(
-    '#media_link_button_container_top'
-  )
-  if (!element_) return EMPTY_LINKS
-
-  const linksString = element_.dataset.links
-  if (!linksString) return EMPTY_LINKS
-
-  /* eslint-disable */
-  const linksData = JSON.parse(linksString)
-
-  const e: { [service: string]: string } = {}
-  for (const a in linksData) {
-    const n = linksData[a]
-    const r = f(a, n, streamingPreferences)
-    r && (e[a] = m(a, r))
+  for (const r in linkData) {
+    const index = linkData[r]
+    if (index.default) (a = index), (n = r)
+    else if (
+      index.for &&
+      index.for.includes(streamingPreferences.service_regions[service])
+    )
+      return (index.media_id = r), index
   }
-  /* eslint-enable */
 
-  return Object.fromEntries(SEARCHABLES.map(({ id }) => [id, e[id]])) as Record<
-    ServiceId,
-    string | undefined
-  >
+  return a
+    ? a.not && a.not.includes(streamingPreferences.service_regions[service])
+      ? null
+      : ((a.media_id = n), a)
+    : null
 }
 
-export type Metadata = { artist: string; title: string }
-export type PageData = { metadata: Metadata; links: Links }
-export const getPageData = async (): Promise<PageData> => {
-  const [artist, title, links] = await Promise.all([
-    getArtist(),
-    getTitle(),
-    getLinks(),
-  ])
-  return { metadata: { artist, title }, links }
+function getFullLink(service: string, linkData: any) {
+  switch (service) {
+    case 'spotify':
+      return `https://open.spotify.com/${linkData.type}/${linkData.media_id}`
+    case 'applemusic':
+      return `https://geo.music.apple.com/${linkData.loc}/${
+        linkData.album ? 'album' : 'video'
+      }/${linkData.album ? linkData.album : linkData.video}/${
+        linkData.media_id
+      }`
+    case 'soundcloud':
+      return `https://${linkData.url}`
+    case 'bandcamp':
+      return `https://${linkData.url}`
+    case 'youtube':
+      return `https://www.youtube.com/watch?v=${linkData.media_id}`
+    default:
+      throw new Error(`Cannot create links for service: ${service}`)
+  }
 }
